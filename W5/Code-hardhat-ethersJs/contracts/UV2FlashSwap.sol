031a2dde4b3dc24fe074742ab47a699128c51f68
ferrarif1 <15858188446@163.com>
2022-03-24 11:47:09 +0800

    套利

--- W5/Code-hardhat-ethersJs/contracts/UV2FlashSwap.sol
+++ W5/Code-hardhat-ethersJs/contracts/UV2FlashSwap.sol
@@ -50,8 +50,7 @@ contract UV2FlashSwap is IUniswapV2Callee {
 
 
     function testFlashLoan(address _tokenBorrow, uint256 _amount)public{
-        MTT.approve(address(this), uint(100000000000000000000000000));
-        FUSD.approve(address(this), uint(100000000000000000000000000));
+      
         MTT.approve(address(uv2router), uint(100000000000000000000000000));
         FUSD.approve(address(uv2router), uint(100000000000000000000000000));
         MTT.approve(address(uv3router), uint(100000000000000000000000000));
@@ -62,16 +61,8 @@ contract UV2FlashSwap is IUniswapV2Callee {
         uint256 amount0Out = _tokenBorrow == token0 ? _amount : 0;
         uint256 amount1Out = _tokenBorrow == token1 ? _amount : 0;
 
-        if(_tokenBorrow == MTTaddress){
-            bool success1 = MTT.transferFrom(msg.sender, address(this), _amount);
-            require(success1, "MTT transfer error!");
-        }else{
-            bool success2 = FUSD.transferFrom(msg.sender, address(this), _amount);
-            require(success2, "FUSD transfer error!");
-        }
-
         //V2:0.1 MTT->0.1 FUSD
-        bytes memory data = abi.encode(MTTaddress, _amount);
+        bytes memory data = abi.encode(_tokenBorrow, _amount);
         IUniswapV2Pair(uv2pair).swap(amount0Out, amount1Out, address(this), data);
     }
 
@@ -84,57 +75,61 @@ contract UV2FlashSwap is IUniswapV2Callee {
     */
 
     function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external override {
-        //assert(msg.sender == UV2Pairaddress);//ensure that msg.sender is actually a V2 pair
-        //console.log("msg.sender = ",msg.sender);
-        //assert(amount0 == 0 || amount1 == 0); 
-        // require(amount0 == 0 || amount1 == 0, "amount0 == 0 || amount1 == 0");
-       console.log("amount1 = ",amount1);
 
+       console.log("amount0,amount1 = ",amount0,amount1);
 
-       address token0 = IUniswapV2Pair(msg.sender).token0();
-       address token1 = IUniswapV2Pair(msg.sender).token1();
        require(msg.sender == UV2Pairaddress, "msg.sender != UV2Pairaddress");
        require(sender == address(this), "ensure that msg.sender is actually a V2 pair");
 
        (address tokenBorrow, uint256 amount) = abi.decode(data, (address, uint256));
-
-       uint256 fee = ((amount * 3) / 997) + 1;
-       uint256 amountRequired = amount + fee;
-
         address[] memory path = new address[](2);
-        path[0] = address(token0);
-        path[1] = address(token1);
-        
-        
-        /*
-         struct ExactInputSingleParams {
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        address recipient;
-        uint256 amountIn;
-        uint256 amountOutMinimum;
-        uint160 sqrtPriceLimitX96;
-    }
-
-    /// @notice Swaps `amountIn` of one token for as much as possible of another token
-    /// @dev Setting `amountIn` to 0 will cause the contract to look up its own balance,
-    /// and swap the entire amount, enabling contracts to send tokens before calling this function.
-    /// @param params The parameters necessary for the swap, encoded as `ExactInputSingleParams` in calldata
-    /// @return amountOut The amount of the received token
-        */
-        //  uint amountOut = UniswapV2Library.getAmountsIn(UniswapV2Factoryaddress, amount, path)[0];
+        uint amountMTT;
+        uint amountFUSD;
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+        assert(amount0 == 0 || amount1 == 0);
+        path[0] = amount0 == 0 ? token0 : token1;
+        path[1] = amount0 == 0 ? token1 : token0;
+        amountMTT = token0 == address(MTTaddress) ? amount0 : amount1;
+        amountFUSD = token0 == address(FUSDaddress) ? amount0 : amount1;
+        //https://docs.uniswap.org/protocol/V2/reference/smart-contracts/library
+        uint amountShouldIN;
         uint256 amountReceived;
-        uint256 amountin = amountRequired - 3000;
-        if(amount0 > 0){
-           IV3SwapRouter.ExactInputSingleParams memory param = IV3SwapRouter.ExactInputSingleParams(FUSDaddress, MTTaddress, 3000, address(this), amountRequired, 0, 0);
+       
+        if(amountFUSD > 0){
+           FUSD.approve(address(SwapRouter02address), uint(100000000000000000000000000));
+           amountShouldIN = UniswapV2Library.getAmountsIn(UniswapV2Factoryaddress, amountFUSD, path)[0];//MTT数量
+           IV3SwapRouter.ExactInputSingleParams memory param = IV3SwapRouter.ExactInputSingleParams(FUSDaddress, MTTaddress, 3000, address(this), amount, 0, 0);
            amountReceived = uv3router.exactInputSingle(param);
         }else{
-           IV3SwapRouter.ExactInputSingleParams memory param = IV3SwapRouter.ExactInputSingleParams(MTTaddress, FUSDaddress, 3000, address(this), amountRequired, 0, 0);
+           MTT.approve(address(SwapRouter02address), uint(100000000000000000000000000));
+           amountShouldIN = UniswapV2Library.getAmountsIn(UniswapV2Factoryaddress, amountMTT, path)[0];//FUSD数量
+           IV3SwapRouter.ExactInputSingleParams memory param = IV3SwapRouter.ExactInputSingleParams(MTTaddress, FUSDaddress, 3000, address(this), amount, 0, 0);
            amountReceived = uv3router.exactInputSingle(param);
         }
-        IERC20(tokenBorrow).transfer(UV2Pairaddress, amountRequired);//address(this) = UV2Pairaddress 
-        IERC20(tokenBorrow).transfer(sender, amountReceived-amountRequired);
+        require(amountReceived > amountShouldIN, "amountReceived <= amountShouldIN");
+        IERC20(tokenBorrow).transfer(UV2Pairaddress, amountShouldIN);//address(this) = UV2Pairaddress 
+        IERC20(tokenBorrow).transfer(sender, amountReceived-amountShouldIN);
+    }
+
+
+    function testUV3(uint8 choose, uint256 amount) public returns(uint256){
+        MTT.approve(address(this), uint(100000000000000000000000000));
+        FUSD.approve(address(this), uint(100000000000000000000000000));
+        MTT.approve(address(uv3router), uint(100000000000000000000000000));
+        FUSD.approve(address(uv3router), uint(100000000000000000000000000));
+        uint256 amountReceived;
+        if(choose == 1){
+           FUSD.transferFrom(msg.sender, address(this), amount);
+           IV3SwapRouter.ExactInputSingleParams memory param = IV3SwapRouter.ExactInputSingleParams(FUSDaddress, MTTaddress, 3000, address(this), amount, 0, 0);
+           amountReceived = uv3router.exactInputSingle(param);
+        }else{
+           MTT.transferFrom(msg.sender, address(this), amount);
+           IV3SwapRouter.ExactInputSingleParams memory param = IV3SwapRouter.ExactInputSingleParams(MTTaddress, FUSDaddress, 3000, address(this), amount, 0, 0);
+           amountReceived = uv3router.exactInputSingle(param);
+        }
+        console.log("amountReceived = ",amountReceived);
+        return amountReceived;
     }
 
 
